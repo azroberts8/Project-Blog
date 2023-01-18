c82f8dec5c046b283b0c05fb171ab7da98ac3e8a
Andrew Roberts <andrew.zane.roberts@gmail.com>
2023-01-18 11:21:09 -0500

    Updated arduino reduced flicker pwm

--- arduino_interrupts/dim_led_reduced_flicker/dim_led_reduced_flicker.ino
+++ arduino_interrupts/dim_led_reduced_flicker/dim_led_reduced_flicker.ino
@@ -1,39 +1,36 @@
 // Control pin 11 LED brightness with brightness variable
 // Modified ISR to reduce flicker for values below 50%
-// Brightness range: 0 (dim) - 100 (max)
-// Cycle period: ~100Hz
+// Brightness range: 0 (off) - 255 (max)
+// Cycle period: ~120Hz
 
-char brightness = 100;
-char count = 1;
-char ticks = 0;
+unsigned char brightness = 10;
+unsigned char count = 1;
+unsigned char ticks = 0;
 
 void setup() {
   DDRB |= (1 << PORTB3);
   cli(); // stop interrupts
 
-  // set timer0 interrupt at 12.8KHz
+  // set timer0 interrupt at ~30.7KHz
   TCCR0A = 0b00000010;
   TCCR0B = 0b00000010; // 8 prescaler
   TCNT0 = 0; // set timer to 0
-  OCR0A = 157;
-  TIMSK0 |= (1 << OCIE0A); // enable timer compare interrupt
+  OCR0A = 64;
+  TIMSK0 |= (1 << OCIE0A); // enable timer0 compare interrupt
 
   sei(); // resume interrupts
 }
 
 ISR(TIMER0_COMPA_vect) {
-  if(count == 100) {
-    count = 0;
-    ticks = 0;
-  }
-
-  if((ticks * 100) / count < brightness) {
+  if((ticks * 255) / count < brightness) {
     PORTB |= (1 << PORTB3);
     ticks++;
-  } else {
-    PORTB &= ~(1 << PORTB3);
-  }
-  count++;
+  } else PORTB &= ~(1 << PORTB3);
+
+  if(count == 255) {
+    count = 1;
+    ticks = 0;
+  } else count++;
 }
 
 void loop() {
